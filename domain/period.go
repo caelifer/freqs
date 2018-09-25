050d08b44f6da84af9e33b9118f04438e9f30ed9
Timour Ezeev <timour.ezeev@me.com>
2018-09-24 20:47:28 -0400

    Split definition of damain objects to their own files

---
+++ domain/period.go
@@ -0,0 +1,68 @@
+package domain
+
+import (
+	"bytes"
+	"strconv"
+)
+
+type Period float64
+
+const (
+	Nanosecond  Period = 1
+	Microsecond        = 1000 * Nanosecond
+	Millisecond        = 1000 * Microsecond
+	Second             = 1000 * Millisecond
+	Minute             = 60 * Second
+	Hour               = 60 * Minute
+	Day                = 24 * Hour
+	Week               = 7 * Day
+)
+
+func (p Period) String() string {
+	if p >= Second {
+		return convertMacroTime(p)
+	}
+
+	// Default is nanosecond precision
+	unit := "ns"
+
+	switch {
+	case p >= Millisecond:
+		unit = "ms"
+		p /= Millisecond
+	case p >= Microsecond:
+		unit = "µs"
+		p /= Microsecond
+	}
+
+	return strconv.FormatFloat(float64(p), 'f', -1, 64) + unit
+}
+
+func convertMacroTime(p Period) string {
+	var buf bytes.Buffer
+	if p >= Week {
+		weeks := int(p / Week)
+		buf.WriteString(strconv.Itoa(weeks) + "w")
+		p -= Period(weeks) * Week
+	}
+	if p >= Day {
+		days := int(p / Day)
+		buf.WriteString(strconv.Itoa(days) + "d")
+		p -= Period(days) * Day
+	}
+	if p >= Hour {
+		hours := int(p / Hour)
+		buf.WriteString(strconv.Itoa(hours) + "h")
+		p -= Period(hours) * Hour
+	}
+	if p >= Minute {
+		minutes := int(p / Minute)
+		buf.WriteString(strconv.Itoa(minutes) + "m")
+		p -= Period(minutes) * Minute
+	}
+
+	seconds := p / Second
+	buf.WriteString(strconv.FormatFloat(float64(seconds), 'f', -1, 64))
+
+	return buf.String() + "s"
+}
---
+++ domain/period_test.go
@@ -0,0 +1,50 @@
+package domain
+
+import (
+	"testing"
+)
+
+func Test__Period_String(t *testing.T) {
+	testCases := []struct {
+		period   Period
+		expected string
+	}{
+		{
+			1 * Minute,
+			"1m0s",
+		}, {
+			100 * Second,
+			"1m40s",
+		}, {
+			100 * Minute,
+			"1h40m0s",
+		}, {
+			10*Hour + 23*Minute + 50*Second + 20*Millisecond,
+			"10h23m50.02s",
+		}, {
+			7*Week + 10*Hour + 23*Minute + 50*Second + 20*Millisecond,
+			"7w10h23m50.02s",
+		}, {
+			5000 * Nanosecond,
+			"5µs",
+		}, {
+			1*Week + 5000*Microsecond,
+			"1w0.005s",
+		}, {
+			1 * Millisecond,
+			"1ms",
+		}, {
+			1 * Microsecond,
+			"1µs",
+		}, {
+			1 * Nanosecond,
+			"1ns",
+		},
+	}
+
+	for _, tc := range testCases {
+		if res := tc.period.String(); res != tc.expected {
+			t.Errorf("for Period(%v) expecting: %v but getting: %v", tc.period, tc.expected, res)
+		}
+	}
+}
